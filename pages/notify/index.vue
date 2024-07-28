fa6b67a4c1a3fdec89a02bf575464c14b13ddd7c
liukun <13655914562@163.com>
2024-07-29 01:57:30 +0800

    fix:init

---
+++ pages/notify/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		<div class="box"></div>
+	</view>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+	
+});
+</script>
+
+<style lang="scss">
+.box {
+	width: 200rpx;
+	height: 200rpx;
+	background-color: pink;
+}
+</style>
