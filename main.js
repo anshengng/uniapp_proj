6b3ff41f509cc1629d3c859050c524a9e1711869
liukun <2871938637@qq.com>
2024-08-06 20:31:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 import App from './App'
 import uviewPlus from '@/uni_modules/uview-plus'
-
-// #ifndef VUE3
-import Vue from 'vue'
-import './uni.promisify.adaptor'
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-	...App
-})
-app.$mount()
-// #endif
-
-// #ifdef VUE3
 import {
 	createSSRApp
 } from 'vue'
+
+
 export function createApp() {
 	const app = createSSRApp(App)
 	app.use(uviewPlus)
@@ -23,4 +12,3 @@ export function createApp() {
 		app
 	}
 }
-// #endif
